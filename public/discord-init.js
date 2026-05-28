e9be0f5ef4e1b32c3f2d1f80ba66ac839cb02b6c
Aliona Vandamme <aliona.vandamme974@gmail.com>
2026-05-28 20:55:33 +0400

    Update discord-init.js

--- public/discord-init.js
+++ public/discord-init.js
@@ -14,3 +14,25 @@ async function setupDiscord() {
 
 // Lancement au démarrage de la page
 setupDiscord();
+
+async function envoyerScoreDansChat(pseudo, essais, grilleVisuelle) {
+    const webhookUrl = "https://discord.com/api/webhooks/1509600182965243904/DoJZYzUl5ulGWSYIZKjOt7LId7aQStHrmdZILOihTWB_nZiyJylk0_APpC2M62D8CWej";
+    
+    // Le message que le "bot" va envoyer dans le salon textuel
+    const payload = {
+        username: "Le Mot FR", // Nom du bot virtuel
+        avatar_url: "public/apple-touch-icon.png",
+        content: `**${pseudo}** vient de terminer Le Mot en **${essais}/6** essais !\n\n${grilleVisuelle}`
+    };
+
+    try {
+        await fetch(webhookUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload)
+        });
+        console.log("Score envoyé dans Discord !");
+    } catch (error) {
+        console.error("Erreur lors de l'envoi du score :", error);
+    }
+}
